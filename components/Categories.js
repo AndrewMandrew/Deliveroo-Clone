11d987f210062e6c6946e70852ad7d2554c484f7
AndrewMandrew <mandressi.andrea@gmail.com>
2023-12-07 17:31:06 +0100

    Improve Categories code with a prop mapping

--- components/Categories.js
+++ components/Categories.js
@@ -2,6 +2,34 @@ import { ScrollView, Text, View } from 'react-native'
 import React, { Component } from 'react'
 import CategoryCard from './CategoryCard'
 
+const CategoriesData = [
+  {
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Sushi_platter.jpg/800px-Sushi_platter.jpg",
+    title: "Sushi",
+  },
+  {
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+    title: "Pizza",
+  },
+  {
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Kebab.JPG/1200px-Kebab.JPG",
+    title: "Kebab"
+  },
+  {
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Sushi_platter.jpg/800px-Sushi_platter.jpg",
+    title: "Sushi",
+  },
+  {
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg",
+    title: "Pizza",
+  },
+  {
+    imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Kebab.JPG/1200px-Kebab.JPG",
+    title: "Kebab"
+  },
+
+]
+
 export class Categories extends Component {
   render() {
     return (
@@ -13,30 +41,11 @@ export class Categories extends Component {
       horizontal={true}
       showsHorizontalScrollIndicator={false}>
         {/* Category card */}
+
         <CategoryCard 
-          imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Sushi_platter.jpg/800px-Sushi_platter.jpg" 
-          title="Sushi" 
+          data={CategoriesData}
         />
-        <CategoryCard 
-          imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg" 
-          title="Pizza" 
-        />
-        <CategoryCard 
-          imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Kebab.JPG/1200px-Kebab.JPG" 
-          title="Kebab" 
-        />
-        <CategoryCard 
-          imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Sushi_platter.jpg/800px-Sushi_platter.jpg" 
-          title="Sushi" 
-        />
-        <CategoryCard 
-          imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg" 
-          title="Pizza" 
-        />
-        <CategoryCard 
-          imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Kebab.JPG/1200px-Kebab.JPG" 
-          title="Kebab" 
-        />
+       
       </ScrollView>
     )
   }
--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,18 +1,20 @@
 import { Text, TouchableOpacity, Image} from 'react-native'
 import React, { Component } from 'react'
 
-const CategoryCard = ({imgUrl, title}) => {
-    return (
-      <TouchableOpacity className="relative mr-2">
-        <Image 
-          source={{
-            uri: imgUrl
-          }}
-          className="h-20 w-20 rounded"
-        />
-        <Text className="absolute bottom-1 left-1 text-white font-bold">{title}</Text>
-      </TouchableOpacity>
-    )
+const CategoryCard = ( props ) => {
+  const categoryItems = props.data.map((item, i) => (
+        <TouchableOpacity key={i} className="relative mr-2">
+          <Image 
+            source={{
+              uri: item.imgUrl
+            }}
+            className="h-20 w-20 rounded"
+          />
+          <Text className="absolute bottom-1 left-1 text-white font-bold">{item.title}</Text>
+        </TouchableOpacity>
+    ));
+
+    return <>{categoryItems}</>
 }
 
 export default CategoryCard
